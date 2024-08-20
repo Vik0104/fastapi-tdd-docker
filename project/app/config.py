200f932035f4c0c8e6ffb52a2663adbebbeef6ef
Shrikanth Iyangar <shrikanthmohan@gmail.com>
2024-08-20 16:57:11 +0530

    tdd fastapi project start

---
+++ project/app/config.py
@@ -0,0 +1,16 @@
+import logging 
+from functools import lru_cache
+
+from pydantic_settings import BaseSettings # type: ignore
+
+log = logging.getLogger("uvicorn")
+
+class Settings(BaseSettings):
+    environment: str = "dev"
+    testing: bool = bool(0)
+
+@lru_cache
+def get_settings() -> BaseSettings:
+    log.info("Loading config settings from the environment...")
+    return Settings()
+
