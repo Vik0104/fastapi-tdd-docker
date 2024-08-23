4eb9ea1fec0e88d242907ab02af72e0b3f3686db
Shrikanth Iyangar <shrikanthmohan@gmail.com>
2024-08-23 14:21:29 +0530

    Updated test cases for remaining routes

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -1,9 +1,9 @@
 from typing import List
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Path
 
 from app.api import crud
-from app.models.pydantic import SummaryPayloadSchema, SummaryResponseSchema
+from app.models.pydantic import SummaryPayloadSchema, SummaryResponseSchema, SummaryUpdatePayloadSchema
 from app.models.tortoise import SummarySchema
 
 router = APIRouter()
@@ -18,9 +18,8 @@ async def create_summary(payload: SummaryPayloadSchema) -> SummaryResponseSchema
 
 
 @router.get("/{id}/", response_model=SummarySchema)
-async def read_summary(id: int) -> int:
+async def read_summary(id: int = Path(..., gt=0)):
     summary = await crud.get(id)
-
     if not summary:
         raise HTTPException(status_code=404, detail="Summary not found")
 
@@ -30,3 +29,32 @@ async def read_summary(id: int) -> int:
 @router.get("/", response_model=List[SummarySchema])
 async def read_all_summaries() -> List[int]:
     return await crud.get_all()
+
+
+@router.delete("/{id}/", response_model=SummaryResponseSchema)
+async def delete_summary(id: int) -> SummaryResponseSchema:
+    summary = await crud.get(id)
+    if not summary:
+        raise HTTPException(status_code=404, detail="Summary not found")
+
+    await crud.delete(id)
+
+    return summary
+
+# @router.put("/{id}/", response_model=SummarySchema)
+# async def update_summary(id: int, payload: SummaryUpdatePayloadSchema):
+#     summary = await crud.put(id, payload)
+#     if not summary:
+#         raise HTTPException(status_code=404, detail="Summary not found")
+
+#     return summary
+
+@router.put("/{id}/", response_model=SummarySchema)
+async def update_summary(id: int, payload: SummaryUpdatePayloadSchema):
+    try:
+        summary = await crud.put(id, payload)
+        if not summary:
+            raise HTTPException(status_code=404, detail="Summary not found")
+        return summary
+    except ValueError as e:
+        raise HTTPException(status_code=422, detail=str(e))
--- project/tests/test_summaries.py
+++ project/tests/test_summaries.py
@@ -53,3 +53,105 @@ def test_read_all_summaries(test_app_with_db):
 
     response_list = response.json()
     assert len(list(filter(lambda d: d["id"] == summary_id, response_list))) == 1
+    
+    
+def test_remove_summary(test_app_with_db):
+    response = test_app_with_db.post(
+        "/summaries/", content=json.dumps({"url": "https://foo.bar"})
+    )
+    summary_id = response.json()["id"]
+
+    response = test_app_with_db.delete(f"/summaries/{summary_id}/")
+    assert response.status_code == 200
+    assert response.json() == {"id": summary_id, "url": "https://foo.bar"}
+
+
+def test_remove_summary_incorrect_id(test_app_with_db):
+    response = test_app_with_db.delete("/summaries/999/")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Summary not found"
+
+
+def test_update_summary(test_app_with_db):
+    response = test_app_with_db.post(
+        "/summaries/", content=json.dumps({"url": "https://foo.bar"})
+    )
+    summary_id = response.json()["id"]
+
+    response = test_app_with_db.put(
+        f"/summaries/{summary_id}/",
+        content=json.dumps({"url": "https://foo.bar", "summary": "updated!"})
+    )
+    assert response.status_code == 200
+
+    response_dict = response.json()
+    assert response_dict["id"] == summary_id
+    assert response_dict["url"] == "https://foo.bar"
+    assert response_dict["summary"] == "updated!"
+    assert response_dict["created_at"]
+
+
+def test_update_summary_incorrect_id(test_app_with_db):
+    response = test_app_with_db.put(
+        "/summaries/999/",
+        content=json.dumps({"url": "https://foo.bar", "summary": "updated!"})
+    )
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Summary not found"
+
+def test_update_summary_invalid_json(test_app_with_db):
+    response = test_app_with_db.post(
+        "/summaries/", content=json.dumps({"url": "https://foo.bar"})
+    )
+    summary_id = response.json()["id"]
+
+    response = test_app_with_db.put(
+        f"/summaries/{summary_id}/",
+        content=json.dumps({})
+    )
+    assert response.status_code == 422
+    
+    errors = response.json()["detail"]
+    assert len(errors) == 2
+    assert any(error["loc"] == ["body", "url"] for error in errors)
+    assert any(error["loc"] == ["body", "summary"] for error in errors)
+    assert all(error["type"] == "missing" for error in errors)
+
+
+def test_update_summary_invalid_keys(test_app_with_db):
+    response = test_app_with_db.post(
+        "/summaries/", content=json.dumps({"url": "https://foo.bar"})
+    )
+    summary_id = response.json()["id"]
+
+    response = test_app_with_db.put(
+        f"/summaries/{summary_id}/",
+        content=json.dumps({"url": "https://foo.bar"})
+    )
+    assert response.status_code == 422
+    
+    errors = response.json()["detail"]
+    assert len(errors) == 1
+    assert errors[0]["loc"] == ["body", "summary"]
+    assert errors[0]["type"] == "missing"
+    
+    
+def test_read_summary_incorrect_id(test_app_with_db):
+    response = test_app_with_db.get("/summaries/999/")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Summary not found"
+
+    response = test_app_with_db.get("/summaries/0/")
+    assert response.status_code == 422
+    assert response.json() == {
+        "detail": [
+            {
+                "ctx": {"gt": 0},
+                "input": "0",
+                "loc": ["path", "id"],
+                "msg": "Input should be greater than 0",
+                "type": "greater_than",
+                "url": "https://errors.pydantic.dev/2.5/v/greater_than",
+            }
+        ]
+    }
